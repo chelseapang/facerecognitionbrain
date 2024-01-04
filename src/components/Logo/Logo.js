c85daae603b9fe93e0a2a7a385bfb9e18dd8c672
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-04 14:23:43 -0800

    adding logo with styling

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className='br2 shadow-2'
+                options={{ max: 55 }}
+                style={{
+                    height: '150px',
+                    width: '150px',
+                    background: 'linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%)',
+                }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '17px'}}alt='logo' src={brain} /></div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
