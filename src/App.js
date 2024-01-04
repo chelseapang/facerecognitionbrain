81f6357f6fdad73793a12805ea84958add3bea94
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-04 15:15:10 -0800

    used particles-bg from react to add animated background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="circle" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*
       <FaceRecognition />*/}
