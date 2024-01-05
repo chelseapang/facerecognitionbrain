aee5e6009a2293d6e1a5914f30fb1794490df55c
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-04 19:38:21 -0800

    setting up functionality for detect button

--- src/App.js
+++ src/App.js
@@ -20,20 +20,27 @@ class App extends Component {
     }
   }
 
-  onInputChange= (event) => {
+  onInputChange = (event) => {
     console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
   }
 
   render() {
     return (
       <div className="App">
-      <ParticlesBg type="circle" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm onInputChange={this.onInputChange}/>
-      <FaceRecognition />
-    </div>
+        <ParticlesBg type="circle" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
+      </div>
     );
   }
 }
