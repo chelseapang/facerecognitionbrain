4fb67a914e1947f46886498f9becbfea1877a99d
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-04 19:28:40 -0800

    adding function to accept input into image link form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import React, { Component } from 'react';
+// import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div className="App">
+// const app = new Clarifai.App({
+//   apiKey: 'YOUR API KEY HERE'
+//  });
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange= (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
       <ParticlesBg type="circle" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />*/}
+      <ImageLinkForm onInputChange={this.onInputChange}/>
+      <FaceRecognition />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
