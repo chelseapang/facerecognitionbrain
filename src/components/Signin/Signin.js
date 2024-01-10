9f761654c4d6e2adf06b36365e8bfa716e4739a1
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-09 21:27:16 -0800

    updating links to fetch from deployed server

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://smart-brain-backend-ybij.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
